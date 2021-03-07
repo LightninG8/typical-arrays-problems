a44ca8bb78640b49c883893352c126f5f5ffc903
Алексей <tov.kessler@yandex.ru>
2021-03-07 16:27:18 +0800

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return arguments.length !== 0 && array.length !== 0  ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return arguments.length !== 0 && array.length !== 0 ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return arguments.length !== 0 && array.length !== 0? array.reduce((prev, cur) => prev + cur, 0) / array.length : 0;
 }
